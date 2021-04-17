08ded0f8119c9f465e4ff2279e2e667cd707fbc4
Anusha <anusharp97@gmail.com>
2021-04-17 22:33:46 +0530

    Files updated

---
+++ LargeFileProcessing/src/main/java/com/demo/batch/configuration/ProductProcessor.java
@@ -0,0 +1,21 @@
+package com.demo.batch.configuration;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.demo.batch.dao.entity.Product;
+
+public class ProductProcessor implements ItemProcessor<Product, Product>{
+
+    @Override
+    public Product process(final Product product) {
+        final String name= product.getName();
+        final String sku= product.getSku();
+        final String desc = product.getDescription();
+
+        final Product processedProduct = new Product();
+        processedProduct.setName(name);
+        processedProduct.setSku(sku);
+        processedProduct.setDescription(desc);
+        return processedProduct;
+    }
+}
